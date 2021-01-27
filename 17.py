5c1619772fbb93353e8578fa822a642df049e9f0
LexLive <76833027+LexLive@users.noreply.github.com>
2021-01-27 20:13:31 +0700

    Add files via upload

---
+++ 17.py
@@ -0,0 +1,14 @@
+print("Hello, I'm Lex")
+print('I want to have a family')
+print("We're working with Python")
+
+# print is a built-in function
+# () accept an input, also call arguement, will print the screen
+# we provide string object as an argument to the print function, technical term is call passing 
+
+print(5)
+print(3 + 4)
+print(10-8)
+
+print("3 + 4") # inside "xyz" is string
+print("3" + "4") #concatenation
