d51b75df3dc57e3e9531abc6e1acf27bfb7dbd8e
LexLive <76833027+LexLive@users.noreply.github.com>
2021-01-27 21:46:11 +0700

    Delete 19.py

--- 19.py
+++
@@ -1,7 +0,0 @@
-#The print Function II: Outputting Multiple Arguments
-
-print("A", "B")
-print("C", "D", "E", "F")
-print(5 + 3, 2 - 8) #each value will seperate by comma
-print(5 + 3, 2 - 8, 3 + 1)
-print("Hello", 10, 3.14) #
