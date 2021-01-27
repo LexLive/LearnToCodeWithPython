c88a3e8b68ab44bc6e13cac76216d36195ad07b3
LexLive <76833027+LexLive@users.noreply.github.com>
2021-01-27 22:06:50 +0700

    Add files via upload

---
+++ learn-py/16.py
@@ -0,0 +1,6 @@
+#Everything in Python is Objects
+#Object is Data Structure
+#Object is Data type 
+#Number and Text are two difference data type or Two difference Object
+
+#String is an Object to store Text
