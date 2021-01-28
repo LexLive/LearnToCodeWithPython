d60687e073282515e57d6ce7bf781615da7baad1
LexLive <76833027+LexLive@users.noreply.github.com>
2021-01-28 21:22:43 +0700

    Add files via upload

---
+++ learn-py/36.py
@@ -0,0 +1,11 @@
+#The NameError, ValueError, TypeError, and SyntaxError Exceptions
+
+# first_name = "Lex"
+# print(firstname) #NameError (Variable Error)
+
+# print("5" + 3) #TypeError
+
+# int("xyz") #ValueError
+
+#print("Hello)
+#SyntaxError
---
+++ learn-py/Assignment5.py
@@ -0,0 +1,3 @@
+#Convert Temperature
+convert_temp = int(input("Fahrenheit to Celsius:"))
+print("Temperature in Celsius", round((convert_temp - 32) * 5 / 9, 2))
