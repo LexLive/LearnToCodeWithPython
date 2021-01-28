b62a5002791f9135eed838c309259bffdc7a4ebc
LexLive <76833027+LexLive@users.noreply.github.com>
2021-01-28 21:51:39 +0700

    Add files via upload

---
+++ learn-py/37.py
@@ -0,0 +1,16 @@
+#Intro to Function
+#Function: A collection of 1 or more statements
+
+
+
+def output_text():
+    print("Welcome to the program!")
+    print("It's nice to meet you")
+    print("have a wonderful day programing!")
+    print("-" * 40)
+
+output_text()
+output_text()
+output_text()
+output_text()
+
