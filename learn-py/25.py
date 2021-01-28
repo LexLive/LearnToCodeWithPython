674e2ab6e8b494be89b3caf4dbf8fae25eec5d7b
LexLive <76833027+LexLive@users.noreply.github.com>
2021-01-28 08:53:46 +0700

    Add files via upload

---
+++ learn-py/25.py
@@ -0,0 +1,14 @@
+#Division, Floor Division, and the Modulo Operator
+print(15 / 3)
+print(14 / 3)
+print(14 // 3) # floor division chop off all decimal; int type
+
+print(-14 // 3, " -14/3 floor division")
+print(14 % 3, "Modulo, remainder")
+print("15/3 remainder is", 15 % 3)
+print("16/3 remainder is", 16 % 3)
+
+#smaller number on the left
+print(1 % 3)
+print(2 % 3)
+print(3 % 3)
