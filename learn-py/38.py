9f402d2511ecc30ca8f60c322555dc27ce169f44
LexLive <76833027+LexLive@users.noreply.github.com>
2021-01-28 22:08:17 +0700

    Add files via upload

---
+++ learn-py/38.py
@@ -0,0 +1,21 @@
+#Defining Parameters and Passing Arguments to Function
+#def p(text <-- define parameter, any paremeter name):
+#
+
+def p(text):
+    print(text)
+p("Hello")
+p("Goodbye")
+p("OK")
+
+#p() TypeError
+
+def add(a, b):
+    print("The sum of", a, "and", b, "is", a + b)
+
+add(3, 5)
+add(-4, 7)
+
+#add()
+#add(1)
+#add(3, 5, 7) #TypeError
