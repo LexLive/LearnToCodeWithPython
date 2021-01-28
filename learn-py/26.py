1b7130fe8a188ce83e95ee05e88ae93c9c64e1f0
LexLive <76833027+LexLive@users.noreply.github.com>
2021-01-28 09:26:16 +0700

    Add files via upload

---
+++ learn-py/26.py
@@ -0,0 +1,21 @@
+#The Boolean Data Type, The Equality Operator (==) and Inequality Operator (!=)
+
+#Boolean: A data type that can be either True or False
+
+#Represenr the isea is true.  
+#not string 
+print(True)
+print(False)
+print("-" * 30)
+
+print(5 == 5)
+print(5 == 8)
+print(8.3 == 8.3)
+print(8.3 == 4.1)
+print(5 == "5", "int and string")
+print("-" * 30)
+
+print("ham" == "ham", "compate T or F")
+print("ham" == "bacon")
+print("ham" == "Ham")
+
