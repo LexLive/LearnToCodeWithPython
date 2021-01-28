ce75e063de643f56085e051451ee44ffc3c71c27
LexLive <76833027+LexLive@users.noreply.github.com>
2021-01-28 11:13:58 +0700

    Add files via upload

---
+++ learn-py/29.py
@@ -0,0 +1,32 @@
+#Type conversion with the int, float, and str Function
+print(int(3.14)) # its chop off the decimal component
+
+print(int(3.99))
+print(int("3"))
+print(int(3))
+
+#float
+print(float(5)) #5.0
+print(float("10.32"))
+print(float(5.23))
+
+print('-' * 20)
+
+
+print(str(5.35))
+print(type(str(5.35))) # convert float to str
+print(str("5"))
+print(str("Hello"))
+
+print('-' * 20)
+
+#print(5 + "Hello") #Error Can't add int and str
+
+print(str(5) + "Hello") #convert int to str
+
+print(3 + 3)
+print(4.3 + 5.7)
+print(3 + 5.8)
+
+
+
