7bfff73ed093430e0c9f8411694e0c00d7dacee2
LexLive <76833027+LexLive@users.noreply.github.com>
2021-01-28 20:53:35 +0700

    Add files via upload

---
+++ learn-py/35.py
@@ -0,0 +1,9 @@
+# Collecting User Input with the input Function
+#first_name = input("What's your name? ")
+#print("It's nice to meet you", first_name)
+
+
+num_one = int(input("First number: "))
+num_two = int(input("Second number: "))
+print("Sum of two number:", num_one + num_two)
+
