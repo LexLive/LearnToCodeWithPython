a607a02146e4063a178818763a722e8eac2b0e9f
LexLive <76833027+LexLive@users.noreply.github.com>
2021-01-28 08:20:31 +0700

    Add files via upload

---
+++ learn-py/24.py
@@ -0,0 +1,14 @@
+#24.Mathematical Ezpression
+print(3+4) #whole number
+print(3.5+6.3) #float
+print(10-5)
+print(5 * 3)
+
+print("Ba" + "con")
+print("lolo" * 16)
+print(10 + 3.8)
+print(5 ** 3)
+print(4 ** 4)
+
+print((3 + 5) * 3)
+
