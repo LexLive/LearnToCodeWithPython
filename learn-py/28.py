f92bd29f178ecdb7e22492de1238d90824ba45f2
LexLive <76833027+LexLive@users.noreply.github.com>
2021-01-28 10:01:55 +0700

    Add files via upload

---
+++ learn-py/28.py
@@ -0,0 +1,26 @@
+#Using type Function to find the class of an object 
+
+#an object is an instance of a class
+
+#class is a blueprint from which object are made
+#instance is an oobject made from class
+
+print(type(5))
+print(type(10))
+print(type(3.8))
+print(type(5.0))
+
+print(type("a"))
+print(type("laptop"))
+
+print(type(5)== type(10))
+print(type("computer") == type("laptop"))
+print(type(5) == type(5.0)) #Compare class
+
+print(type(True)) 
+print("-" * 20)
+
+print(type(True) == type(False))
+
+print(type([1, 2, 3]))
+print(type({"Ho": "Koonmei"}))
