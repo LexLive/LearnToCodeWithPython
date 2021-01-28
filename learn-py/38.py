be4271c78c9db8bf1dae345e67422b0fcc8912e3
LexLive <76833027+LexLive@users.noreply.github.com>
2021-01-28 22:28:26 +0700

    Add files via upload

--- learn-py/38.py
+++ learn-py/38.py
@@ -1,6 +1,18 @@
 #Defining Parameters and Passing Arguments to Function
 #def p(text <-- define parameter, any paremeter name):
+#Function is an Object
+#A function can accept inputs called arguments and return outputs called return values.
+#The technical term for an expected input to a function is a parameter.
+#And the actual value we provide as the input when the function is invoked is called the argument.
+#You can think of Def almost like a variable assignment and output text as the name of a function.
+#The function is an object and we are assigning it a name here called output text.
 #
+#the parameter name is completely up to us the developer.
+#This is an input that we're going to provide to p and whatever we provide inside the parentheses will be available within the body or the block that we define the rules of the function and or the steps
+#
+
+#
+# will be available within the body or the block that we define the rules of the function and or the steps
 
 def p(text):
     print(text)
