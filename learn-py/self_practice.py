c4f2ef4065c6d147bbabe5242e6c86657cd4af80
LexLive <76833027+LexLive@users.noreply.github.com>
2021-01-30 17:49:16 +0700

    Update self_practice.py

--- learn-py/self_practice.py
+++ learn-py/self_practice.py
@@ -3,9 +3,6 @@ def sf(great):
 sf("Wanna be Great")
 sf("Wanna be Love")
 
-
-
-
 def none():
     print("anything")
     print("ABCDEF")
@@ -15,9 +12,11 @@ none()
 #programiz youtube
 def add_number(n1, n2):
     result = n1 + n2
-    print("The sum is", round(result,3))
+    #print("The sum is", round(result,3)) #use return instead is better
+    return result
 
 number1 = 5.4
 number2 = 6.7
-add_number(number1, number2)
+result = add_number(number1, number2)
+print("The sum is", result)
 
