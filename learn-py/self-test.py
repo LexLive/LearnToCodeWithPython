a6266889da5cd0ec98a82babc04841a4bb173460
LexLive <76833027+LexLive@users.noreply.github.com>
2021-01-30 18:37:45 +0700

    Create self-test.py

---
+++ learn-py/self-test.py
@@ -0,0 +1,18 @@
+def family(n1, n2, n3):
+    print(f"Welcome home! {n1}, {n2}, {n3}")
+
+family("Koonmei", "Amelia", "AAAA")
+family("P'june", "Lex", "BBBB")
+
+
+
+def math(count1, count2):
+    result = count1 + count2
+    
+    return result
+    
+count1 = 10
+count2 = 10
+
+result = math(count1, count2)
+print("My bank statement: ", result)
