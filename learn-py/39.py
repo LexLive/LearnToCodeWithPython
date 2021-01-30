f33963852f8c3b4c34346b8dc20b9c412e2d23b7
LexLive <76833027+LexLive@users.noreply.github.com>
2021-01-30 19:59:15 +0700

    Create 39.py

---
+++ learn-py/39.py
@@ -0,0 +1,12 @@
+#Positional Arguments and Keywrod Arguments
+
+def add(a, b, c):
+    print("The sum of the three number is", a + b + c)
+
+add(4, 6, 10) #position Argument
+add(a = 5, b = 10, c= 15)#keyword Argument
+add(5, b = 10, c = 15)
+add(5, c = 10, b = 15) #any order
+
+#position Argument
+#keyword Argument
