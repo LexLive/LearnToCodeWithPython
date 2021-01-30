fee9c0f07281c3a68faa8f99d2f346ef3abed256
LexLive <76833027+LexLive@users.noreply.github.com>
2021-01-30 19:59:17 +0700

    Create 40.py

---
+++ learn-py/40.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    return a + b #output #dont
+    print("This is nonesense") #not show in Terminal
+
+result = add(3, 5)
+print(result)
+
+#return a + b #output #dont
+#It is sort of like a self terminating line of code.
+#It just tells the function I'm done running.
+#Take this output and bring it back outside to the outside world.
