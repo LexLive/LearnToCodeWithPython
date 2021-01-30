44eeb1aff20e322e722239f25156171d7769faa4
LexLive <76833027+LexLive@users.noreply.github.com>
2021-01-30 18:10:44 +0700

    Update practice_function_ll.py

--- learn-py/practice_function_ll.py
+++ learn-py/practice_function_ll.py
@@ -1,5 +1,5 @@
 def just_name(name1, name2):
-    print(f"Hello, guy {name1}, {name2}")
-
-just_name("Mei", "Lex")
-just_name("Guang", "Ham")
+    print(f"Welcome Home! {name1}, {name2}")
+    
+just_name("P'june", "Amelia")
+just_name("Mom KoonMei", "Lex")
