550ded92dcb8aea0a7b239a2db7b85fea127cf7c
LexLive <76833027+LexLive@users.noreply.github.com>
2021-01-30 17:45:03 +0700

    Update self_practice.py

--- learn-py/self_practice.py
+++ learn-py/self_practice.py
@@ -1,7 +1,23 @@
-def self_practice():
-    print("I wanna be Great")
+def sf(great):
+    print(great)
+sf("Wanna be Great")
+sf("Wanna be Love")
 
-self_practice()
-self_practice()
-self_practice()
-self_practice()
+
+
+
+def none():
+    print("anything")
+    print("ABCDEF")
+    print("*" * 20)
+none()
+
+#programiz youtube
+def add_number(n1, n2):
+    result = n1 + n2
+    print("The sum is", round(result,3))
+
+number1 = 5.4
+number2 = 6.7
+add_number(number1, number2)
+
