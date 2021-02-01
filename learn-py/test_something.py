5ab62d084f8c75315f5ee1e4bfd1d60f44bb1693
LexLive <76833027+LexLive@users.noreply.github.com>
2021-02-01 21:46:17 +0700

    Create test_something.py

---
+++ learn-py/test_something.py
@@ -0,0 +1,30 @@
+def easy_money():
+    return 100
+    
+easy_money()
+
+
+def best_food_ever():
+    print("Sushi")
+    
+best_food_ever()
+
+def convert_to_currency(a):
+    print(f"${a}")
+    
+convert_to_currency(20)
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+    
