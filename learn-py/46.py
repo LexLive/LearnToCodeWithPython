9d93f5eca0742720405523ee702e4da00aea2bab
LexLive <76833027+LexLive@users.noreply.github.com>
2021-02-03 10:51:08 +0700

    Add files via upload
    Length, concatenation, and immutability

---
+++ learn-py/46.py
@@ -0,0 +1,18 @@
+#lenght, concatenation and immutability
+print(len('Python'))
+print(len('Programing'))
+print(len(' '))
+print(len(''))
+
+#print(len(4)) #error
+#print(len(3.14))
+
+print('lex' + ' ' +'mom')
+print('lex ' + 'amelia')
+
+print('a' 'b' 'c' ) #not recommened
+
+print('--' * 20)
+
+#immutability cannot be change
+
